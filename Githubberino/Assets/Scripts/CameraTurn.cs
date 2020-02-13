6d195a6a14e47af531f53e54ca89ed3d6805e74f
Greenrepeat <61007678+Greenrepeat@users.noreply.github.com>
2020-02-13 20:29:54 +0800

    Movement

---
+++ Githubberino/Assets/Scripts/CameraTurn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTurn : MonoBehaviour;
+  { 
+    public float cameraSpeed = 2f;
+
+}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if (Input.GetKey(KeyCode.RightArrow) == true) { this.transform.position += this.transform.forward * Time.deltaTime * this.cameraSpeed; }
+    if (Input.GetKey(KeyCode.LeftArrow) == true) { this.transform.position -= this.transform.forward * Time.deltaTime * this.cameraSpeed; }
+}
---
+++ Githubberino/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{ 
+
+    public float moveSpeed = 5f;
+    public float turnSpeed = 100f;
+    public float jumpSpeed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetKey( KeyCode.W ) == true ) {   moveSpeed;  }
+        if( Input.GetKey( KeyCode.S ) == true ) { this.transform.position -= this.transform.forward * Time.deltaTime * this.moveSpeed;  }
+
+        if( Input.GetKey( KeyCode.D ) == true ) { this.transform.Rotate(this.transform.up, Time.deltaTime * this.turnSpeed);  }
+        if( Input.GetKey( KeyCode.A ) == true ) { this.transform.Rotate(this.transform.up, Time.deltaTime * -this.turnSpeed); }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.up * jumpSpeed;
+        }
+
+
+    }
+}
