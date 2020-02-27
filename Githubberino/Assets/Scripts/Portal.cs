44748cbe49cff38f04d28be7c5b4b4b44145c85c
Greenrepeat <61007678+Greenrepeat@users.noreply.github.com>
2020-02-27 20:12:28 +0800

    Portal work

--- Githubberino/Assets/Scripts/Portal.cs
+++ Githubberino/Assets/Scripts/Portal.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Portal : MonoBehaviour
+public class Portal : MonoBehaviour { public GameObject Player; public GameObject Destination; }
+
+
+void OnTriggerEnter(Collider other)
+{
+    if (other.gameObject == Player)
+        
+            }
 {
     // Start is called before the first frame update
     void Start()
